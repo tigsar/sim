8abc68c18302977d4c72a8aa2fb0cc417c49e706
Tigran Sargsyan <tigransargsyan@MacBook-Air-de-Tigran.local>
2019-03-13 00:00:05 +0100

    Added missing Sensor, Actuator and Controller models

--- launcher.js
+++ launcher.js
@@ -7,6 +7,20 @@ let angularVelocity = Symbol('thetaD');
 let angularAcceleration = Symbol('thetaDD');
 let deflection = Symbol('beta');
 
+let deflectionAngularVelocity = Symbol('betaD');
+let deflectionAngularAcceleration = Symbol('betaDD');
+let actuatorNaturalFrequency = Symbol('omega_s');
+let actuatorDampingRatio = Symbol('zeta_s');
+let commandedDeflection = Symbol('beta_c');
+
+let controllerGain = Symbol('K_p');
+let reference = Symbol('theta_r');
+
+let scaleFactor = Symbol('s');
+let bias = Symbol('b');
+let noiseVariance = Symbol('sigma');
+let measuredAngle = Symbol('theta_m');
+
 /* Definition of derivatives */
 let derivativeOf = {
     [angle]: angularVelocity,
@@ -55,6 +69,77 @@ class LauncherPlant {
             [angularAcceleration]: ((input[thrust] * this.parameters[arm]) / this.parameters[momentOfInertia]) * Math.sin(input[deflection])
         };
     }
+
+    output(input, derivative) {
+        checkSymbol(input, thrust);
+        checkSymbol(input, deflection);
+        return {
+            [angle]: this.state[angle],
+            [angularVelocity]: this.state[angularVelocity],
+            [angularAcceleration]: derivative[angularAcceleration]
+        };
+    }
+}
+
+class AngleSensor {
+    constructor(parameters) {
+        checkSymbol(parameters, scaleFactor);
+        checkSymbol(parameters, bias);
+        checkSymbol(parameters, noiseVariance);
+        this.parameters = parameters;
+    }
+
+    output(input) {
+        checkSymbol(input, angle);
+        return {
+            [measuredAngle]: input[angle] * this.parameters[scaleFactor] + this.parameters[bias] + Math.sqrt(this.parameters[noiseVariance]) * Math.random()
+        };
+    }
+}
+
+class NozzleActuator {
+    constructor(parameters, initialCondition) {
+        checkSymbol(parameters, actuatorNaturalFrequency);
+        checkSymbol(parameters, actuatorDampingRatio);
+        this.parameters = parameters;
+
+        checkSymbol(initialCondition, deflection);
+        checkSymbol(initialCondition, deflectionAngularVelocity);
+        this.state = initialCondition;
+        this.time = 0;
+    }
+
+    derivative(input) {
+        checkSymbol(input, commandedDeflection);
+        let naturalFrequencySquare = Math.pow(this.parameters[actuatorNaturalFrequency], 2);
+        return {
+            [deflectionAngularVelocity]: this.state[deflectionAngularVelocity],
+            [deflectionAngularAcceleration]: naturalFrequencySquare * input[commandedDeflection] - naturalFrequencySquare * this.state[deflection] - 2 * this.parameters[actuatorNaturalFrequency] * this.parameters[actuatorDampingRatio] * this.state[deflectionAngularVelocity]
+        };
+    }
+
+    output(input, derivative) {
+        checkSymbol(input, commandedDeflection);
+        return {
+            [deflection]: this.state[deflection]
+        };
+    }
+}
+
+class ProportionalController {
+    constructor(parameters) {
+        checkSymbol(parameters, controllerGain);
+        this.parameters = parameters;
+        this.time = 0;
+    }
+
+    output(input, derivative) {
+        checkSymbol(input, angle);
+        checkSymbol(input, reference);
+        return {
+            [commandedDeflection]: this.parameters[controllerGain] * (input[angle] - input[reference])
+        };
+    }
 }
 
 let launcher = new LauncherPlant({
@@ -65,10 +150,30 @@ let launcher = new LauncherPlant({
     [angularVelocity]: 0.1
 });
 
+let sensor = new AngleSensor({
+    [scaleFactor]: 1.01,
+    [bias]: 0.1,
+    [noiseVariance]: 0.1
+});
+
+let actuator = new NozzleActuator({
+    [actuatorNaturalFrequency]: 80,
+    [actuatorDampingRatio]: 0.7,
+}, {
+    [deflection]: 0,
+    [deflectionAngularVelocity]: 0
+});
+
+let controller = new ProportionalController({
+    [controllerGain]: 1
+});
+
+/* Models wiring */
+
 let integartor = new SimpleIntegrator(launcher, 0.01);
 integartor.integrate({
     [thrust]: 100,
     [deflection]: 0.001
 });
 
-console.log(launcher.state)
+console.log(launcher.state);
