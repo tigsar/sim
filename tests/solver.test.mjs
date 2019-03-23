be6e2ffe962c4d03ed9784496a311c84d6114b73
Tigran Sargsyan <tigransargsyan@MacBook-Air-de-Tigran.local>
2019-03-23 14:15:34 +0100

    Added a test that checks algebraic loop detection function

---
+++ tests/solver.test.mjs
@@ -0,0 +1,15 @@
+import * as Tf from './models/tf';
+import {Solver, PressenceOfAlgebraicLoop} from './simulation';
+
+const inputSignal = Symbol('Input signal');
+const outputSignal = Symbol('Output signal');
+
+test('Detection of algebraic loop', () => {
+    let block = new Tf.Block("Test", inputSignal, outputSignal, [ 1, -1 ], [ 1 ]);
+    expect(() => {
+        new Solver([ block ], [{
+            from: { block: block, signal: outputSignal },
+            to: { block: block, signal: inputSignal }
+        }], 0.125);
+    }).toThrow(PressenceOfAlgebraicLoop);
+});
