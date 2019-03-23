bea97d34bd1b8b0adaf147058e8bc9df9594cd03
Tigran Sargsyan <tigransargsyan@MacBook-Air-de-Tigran.local>
2019-03-23 14:19:52 +0100

    Check that there is no algebraic loop when an integrator's output is connected to its input

--- tests/solver.test.mjs
+++ tests/solver.test.mjs
@@ -13,3 +13,13 @@ test('Detection of algebraic loop', () => {
         }], 0.125);
     }).toThrow(PressenceOfAlgebraicLoop);
 });
+
+test('No algebraic loop is detected for integrator with feedback', () => {
+    let block = new Tf.Block("Test", inputSignal, outputSignal, [ 0, 1 ], [ 1 ]);
+    expect(() => {
+        new Solver([ block ], [{
+            from: { block: block, signal: outputSignal },
+            to: { block: block, signal: inputSignal }
+        }], 0.125);
+    }).not.toThrow(PressenceOfAlgebraicLoop);
+});
